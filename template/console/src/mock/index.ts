a070c8e90e1d9135275be900ab22d8b34dd8ca26
virgoone <me@virgo.one>
2022-01-02 21:06:53 +0800

    feat: 修改模版为arco pro模版

---
+++ template/console/src/mock/index.ts
@@ -0,0 +1,8 @@
+import Mock from 'mockjs'
+
+import './user'
+import './policy'
+
+Mock.setup({
+  timeout: '200-600'
+})
--- template/console/types/custom-typings.d.ts
+++ template/console/types/custom-typings.d.ts
@@ -20,12 +20,46 @@ declare const SENTRY_DSN: string
 declare const APP_ENV: string
 declare const SENTRY_RELEASE: string
 
-declare module '*.less'
-
-declare module '*.png'
-
-declare module '*.less?useable'
-
 declare module 'penv.macro'
 
 declare module 'debug'
+
+declare module '*.svg' {
+  const content: any
+  export default content
+}
+
+declare module '*.less' {
+  const classes: { [className: string]: string }
+  export default classes
+}
+declare module '*.less?modules' {
+  const classes: { [className: string]: string };
+  export default classes;
+}
+declare module '*.scss' {
+  const classes: { [className: string]: string }
+  export default classes
+}
+declare module '*.scss?modules' {
+  const classes: { [className: string]: string };
+  export default classes;
+}
+
+declare module '*/settings.json' {
+  const value: {
+    colorWeek: boolean
+    navbar: boolean
+    menu: boolean
+    footer: boolean
+    themeColor: string
+    menuWidth: number
+  }
+
+  export default value
+}
+
+declare module '*.png' {
+  const value: string
+  export default value
+}

5f965c7c4005764c17ed4a39d50666f25c704547
virgoone <me@virgo.one>
2022-01-13 17:04:55 +0800

    feat: 增加基础详情页面模版

---
+++ template/console/src/pages/profile/basic/item.tsx
@@ -0,0 +1,128 @@
+import React, { CSSProperties } from 'react'
+import useLocale from '@/hooks/useLocale'
+import { Descriptions, Card, Skeleton } from '@arco-design/web-react'
+import locales from './locale'
+
+interface ProfileItemProps {
+  title: string
+  data: any
+  style?: CSSProperties
+  type: 'origin' | 'current'
+  loading?: boolean
+}
+
+function ProfileItem(props: ProfileItemProps) {
+  const locale = useLocale(locales)
+  const { data, type, loading } = props
+  const blockDataList: {
+    title: string
+    data: {
+      label: string
+      value: string
+    }[]
+  }[] = []
+
+  blockDataList.push({
+    title: locale[`basicProfile.title.${type}Video`],
+    data: [
+      {
+        label: locale['basicProfile.label.video.mode'],
+        value: data?.video?.mode || '-',
+      },
+      {
+        label: locale['basicProfile.label.video.acquisition.resolution'],
+        value: data?.video?.acquisition.resolution || '-',
+      },
+      {
+        label: locale['basicProfile.label.video.acquisition.frameRate'],
+        value: `${data?.video?.acquisition.frameRate || '-'} fps`,
+      },
+      {
+        label: locale['basicProfile.label.video.encoding.resolution'],
+        value: data?.video?.encoding.resolution || '-',
+      },
+      {
+        label: locale['basicProfile.label.video.encoding.rate.min'],
+        value: `${data?.video?.encoding.rate.min || '-'} bps`,
+      },
+      {
+        label: locale['basicProfile.label.video.encoding.rate.max'],
+        value: `${data?.video?.encoding.rate.max || '-'} bps`,
+      },
+      {
+        label: locale['basicProfile.label.video.encoding.rate.default'],
+        value: `${data?.video?.encoding.rate.default || '-'} bps`,
+      },
+      {
+        label: locale['basicProfile.label.video.encoding.frameRate'],
+        value: `${data?.video?.encoding.frameRate || '-'} fpx`,
+      },
+      {
+        label: locale['basicProfile.label.video.encoding.profile'],
+        value: data?.video?.encoding.profile || '-',
+      },
+    ],
+  })
+
+  blockDataList.push({
+    title: locale[`basicProfile.title.${type}Audio`],
+    data: [
+      {
+        label: locale['basicProfile.label.audio.mode'],
+        value: data?.audio?.mode || '-',
+      },
+      {
+        label: locale['basicProfile.label.audio.acquisition.channels'],
+        value: `${data?.audio?.acquisition.channels || '-'} ${
+          locale['basicProfile.unit.audio.channels']
+        }`,
+      },
+      {
+        label: locale['basicProfile.label.audio.encoding.channels'],
+        value: `${data?.audio?.encoding.channels || '-'} ${
+          locale['basicProfile.unit.audio.channels']
+        }`,
+      },
+      {
+        label: locale['basicProfile.label.audio.encoding.rate'],
+        value: `${data?.audio?.encoding.rate || '-'} kbps`,
+      },
+      {
+        label: locale['basicProfile.label.audio.encoding.profile'],
+        value: data?.audio?.encoding.profile || '-',
+      },
+    ],
+  })
+
+  return (
+    <Card>
+      <div>
+        {blockDataList.map(({ title: blockTitle, data: blockData }, index) => (
+          <Descriptions
+            key={`${index}`}
+            colon=":"
+            labelStyle={{ textAlign: 'right', width: 200, paddingRight: 10 }}
+            valueStyle={{ width: 400 }}
+            title={blockTitle}
+            data={
+              loading
+                ? blockData.map((item) => ({
+                    ...item,
+                    value: (
+                      <Skeleton
+                        text={{ rows: 1, style: { width: '200px' } }}
+                        animation
+                      />
+                    ),
+                  }))
+                : blockData
+            }
+            style={index > 0 ? { marginTop: '20px' } : {}}
+          />
+        ))}
+      </div>
+    </Card>
+  )
+}
+
+export default ProfileItem
